e8a09ba2889e42b3a892c5fa61499822170b011a
nik <nik@hal.com>
2021-05-24 11:22:01 +0300

    correct types

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,14 @@ async function getMoviesNow(apiToken: APIToken): Promise<MovieData> {
   console.log('test')
   console.log(apiToken)
   const fetchUrl = `https://api.themoviedb.org/3/movie/76341?api_key=${apiToken.apiKey}`
-  const movies = await fetch(fetchUrl)
-  const data = await movies.json()
+  const movies: Promise<MovieData> = await (await fetch(fetchUrl)).json()
+  // const data = await movies.json()
   // console.log(data)
-  return data
+  return movies
 }
 
 async function loadDOM(): Promise<void> {
-  const DOMData = await getMoviesNow({ apiKey: 'bc50218d91157b1ba4f142ef7baaa6a0' })
+  const DOMData: MovieData = await getMoviesNow({ apiKey: 'bc50218d91157b1ba4f142ef7baaa6a0' })
   const appEntryPoint: HTMLElement = document.getElementById('app') as HTMLElement
   appEntryPoint.innerText = DOMData.id
   console.log(DOMData.id)
