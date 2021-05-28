06018065543f4cb6899335057d7249f1e24445ef
nik <nik@hal.com>
2021-05-28 19:46:26 +0300

    move app related functions to app class

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { APIToken, MovieDetailsMoreCollection } from './shared/model/model-common'
+import { APIToken, Genre, Movie, MovieDetailsMoreCollection, ReviewsDetails } from './shared/model/model-common'
 import {
   ConfigurationResponse,
   GenreResponse,
@@ -8,6 +8,7 @@ import {
 } from './shared/model/model-results'
 import { MoviesMoreRequest, MoviesNowRequest, MoviesSearchRequest } from './shared/model/model-requests'
 import { ApiRequestService as ApiService } from './shared/services/request-service'
+import { Utils } from './shared/services/utils-service'
 
 export class App {
   private apiService = new ApiService()
@@ -15,6 +16,15 @@ export class App {
   private moviesNowCurentRequest: MoviesNowRequest
   private moviesSearchRequest: MoviesSearchRequest
   private movieMoreDetails: MoviesMoreRequest
+  moviesParentNode: HTMLElement
+  searchBtnNode: HTMLInputElement
+  moviesNowhBtnNode: HTMLButtonElement
+  observerNode: HTMLElement
+  moviesNode: HTMLUListElement
+  genres: Genre[]
+  configObj: string
+
+  utils = new Utils()
 
   constructor(theApitoken: APIToken) {
     this.myApiToken = theApitoken
@@ -31,6 +41,25 @@ export class App {
       apiKey: this.myApiToken.apiKey,
       movieId: '',
     }
+
+    // get references to dom nodes
+    this.moviesParentNode = document.getElementById('movies') as HTMLElement
+    this.searchBtnNode = document.getElementById('movies-search') as HTMLInputElement
+    this.moviesNowhBtnNode = document.getElementById('movies-now') as HTMLButtonElement
+    this.observerNode = document.getElementById('infinite-scroll-trigger') as HTMLElement
+    this.moviesNode = document.createElement('ul')
+    // the dynamic part of the page, append it to it's parent element
+    this.moviesParentNode.appendChild(this.moviesNode)
+    /**
+     ** get the configuration data which might be needed. #todo: store in localstorage, as suggested
+     */
+    // this.configObjPromise = configObjPromise//await this.getConfig(this.anApiToken)
+    // this.configObj = this.configObjPromise.images.base_url
+    /**
+     * get the genres to lookup
+     */
+    this.configObj = ''
+    this.genres = []
   }
 
   public get anApiToken(): APIToken {
@@ -49,14 +78,7 @@ export class App {
     return this.movieMoreDetails
   }
 
-  // public set moviesMoreDetails(_theId) {
-  //   this.movieMoreDetails.movieId = _theId.movieId
-  // }
-  public setmovieMoreDetails(_id: string = '') {
-    this.movieMoreDetails.movieId = _id
-  }
   /**
-  
    *
    * @param params the provided user API token
    * @returns the configuration of the site
@@ -80,6 +102,19 @@ export class App {
     return await this.apiService.getMovieDetails(params)
   }
 
+  public setmovieMoreDetails(_id: string = '') {
+    this.movieMoreDetails.movieId = _id
+  }
+
+  public _getConfig = async () => {
+    const configObjPromise: ConfigurationResponse = await this.getConfig(this.anApiToken)
+    this.configObj = configObjPromise.images.base_url
+  }
+
+  public _getGenres = async () => {
+    this.genres = (await this.getGenres(this.anApiToken)).genres
+  }
+
   public async getMovieMore(params: MoviesMoreRequest): Promise<MovieDetailsMoreCollection> {
     const [_reviews, _similar, _videos] = await Promise.all([
       this.apiService.getMovieDetailsReviews(params),
@@ -92,10 +127,144 @@ export class App {
       videos: _videos,
     }
   }
-  // public async loadDOM(): Promise<void> {
-  //   const DOMData = await this.getMoviesNow({ apiKey: '1d342f36ce9ad0f75afa27e2eb307f5805f100f2' })
-  //   const appEntryPoint: HTMLElement = document.getElementById('app') as HTMLElement
-  //   appEntryPoint.innerText = DOMData.id
-  //   console.log(DOMData.id)
-  // }
+
+  /**
+   *
+   * @param _movies the result list from the queries
+   * this builds the DOM initially and everytime there is a new search
+   */
+  private _buildDOMwithResults = (_movies: Movie[]): void => {
+    // document.dispatchEvent(new CustomEvent('movieDataLoaded', { detail: moviesDataResults }))
+    if (this.moviesSearch.query == '') {
+      this.utils._updatePageRequest(this.moviesNowCurent)
+    } else {
+      this.utils._updatePageRequest(this.moviesSearch)
+    }
+
+    // could use a template - #TODO
+    for (const movie of _movies) {
+      const movieLiNode: HTMLLIElement = document.createElement('li')
+      movieLiNode.setAttribute('data-movie-id', movie.id?.toString() || '')
+      movieLiNode.setAttribute('data-movie-link', 'true')
+      this.moviesNode.appendChild(movieLiNode)
+
+      const fragment: DocumentFragment = document.createDocumentFragment()
+      const childDiv1: HTMLElement = document.createElement('div')
+      const childDiv2: HTMLElement = document.createElement('div')
+      const childDiv3: HTMLElement = document.createElement('div')
+
+      childDiv1.setAttribute('class', 'movie-column with-poster')
+      childDiv1.innerHTML = `<img class="movie-poster responsive" loading="lazy" width="500" height="750" src="${this.configObj}w500/${movie.poster_path}"/>`
+      fragment.appendChild(childDiv1)
+
+      childDiv2.setAttribute('class', 'movie-column with-info')
+      childDiv2.innerHTML = `<h1 class="movie-title"><a class="override-link" target="blank" href="https://www.themoviedb.org/movie/${
+        movie.id
+      }">${movie.title?.trim()}</a><span class="movie-date">(${this.utils._getYear(
+        movie.release_date
+      )})</span><span class="movie-more">...more</span></h1><span class="movie-genres">${this.utils._getGenreTitle(
+        movie.genre_ids,
+        this.genres
+      )}</span><p>${movie.overview}</p><p class="movie-stars">${this.utils._getStars(movie.vote_average)}</p>`
+      fragment.appendChild(childDiv2)
+
+      childDiv3.setAttribute('class', 'movie-column with-more-info')
+      fragment.appendChild(childDiv3)
+
+      movieLiNode.appendChild(fragment)
+    }
+  }
+
+  _getMoreMovies = async () => {
+    const moviesDataPromise = await this.getMoviesNow(this.moviesNowCurent)
+    const moviesDataResults = moviesDataPromise.results
+    if (moviesDataResults) {
+      this._buildDOMwithResults(moviesDataResults)
+    }
+  }
+
+  _getMoreSearchedMovies = async () => {
+    const moviesDataPromise = await this.getMoviesSearch(this.moviesSearch)
+    const moviesDataResults = moviesDataPromise.results
+    if (moviesDataResults) {
+      this._buildDOMwithResults(moviesDataResults)
+    }
+  }
+
+  _addMovieMoreContent = (_data: MovieDetailsMoreCollection, _desired: HTMLElement): void => {
+    let _innerHTML = ''
+    const fragment: DocumentFragment = document.createDocumentFragment()
+    const childDiv: HTMLElement = document.createElement('div')
+    childDiv.setAttribute('class', 'more-info-panel')
+    const { reviews, similar, videos } = _data
+    if (videos.results && videos.results.length > 0) {
+      for (const video of videos.results) {
+        _innerHTML += `<iframe width="420" height="315" src="https://www.${video.site}.com/embed/${video.key}"></iframe>`
+      }
+    }
+    if (reviews.results && reviews.results.length > 0) {
+      _innerHTML += '<div>'
+      _innerHTML += '<h1>Reviews</h1>'
+      const limitedReviews: ReviewsDetails[] = []
+      limitedReviews.push(reviews.results[0])
+      if (reviews.results[1]) limitedReviews.push(reviews.results[1])
+      for (const review of limitedReviews) {
+        _innerHTML += `<h3>from ${review.author} </h3><p>${review.content}</p>`
+      }
+      _innerHTML += '</div>'
+    }
+    if (similar.results && similar.results.length > 0) {
+      _innerHTML += '<div>'
+      _innerHTML += '<h1>Similar movies</h1>'
+      for (const simil of similar.results) {
+        _innerHTML += `<h3><a target="_blank" href="https://www.themoviedb.org/movie/${simil.id}"> ${simil.title}</a> </h3>`
+      }
+      _innerHTML += '</div>'
+    }
+
+    childDiv.innerHTML = _innerHTML
+    fragment.appendChild(childDiv)
+    _desired?.querySelector('.with-more-info')?.appendChild(fragment)
+  }
+  /**
+   *
+   * @param evt
+   * @returns the selected movie details. we will be adding a single event listener which will delegate the event
+   * instead of adding one for every movie which will have a performance penalty. as an eastern egg, you can
+   * click anywhere on the movie and get the results but this will not be indicated
+   *
+   */
+  _showMoreDetails = async (evt: Event): Promise<void | null> => {
+    const _el = evt.target as HTMLElement
+    const _desired: HTMLElement = _el.closest('[data-movie-link]') as HTMLElement
+    const _movieId = _desired?.getAttribute('data-movie-id')
+    let _movieData: MovieDetailsMoreCollection
+    this.setmovieMoreDetails(_movieId || '')
+    if (_desired?.classList.contains('show-more')) {
+      _desired.classList.remove('show-more')
+      return null
+    } else {
+      if (_desired.querySelector('.with-more-info')?.innerHTML == '') {
+        _movieData = await this.getMovieMore(this.moviesMoreDetails)
+        console.log(_movieData)
+        this._addMovieMoreContent(_movieData, _desired)
+      }
+      _desired?.classList.toggle('show-more')
+    }
+  }
+
+  _getSearchedMovies = async (searchString: string): Promise<void> => {
+    this.moviesSearch.query = searchString
+    this.moviesSearch.pageNo = 1
+    const moviesDataPromise = await this.getMoviesSearch(this.moviesSearch)
+    const moviesDataResults = moviesDataPromise.results
+    if (moviesDataResults && moviesDataResults.length > 0) {
+      this.moviesNode.innerHTML = ''
+      this._buildDOMwithResults(moviesDataResults)
+    } else {
+      this.moviesSearch.query = ''
+      this.moviesSearch.pageNo = 1
+      alert('no results')
+    }
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,210 +1,34 @@
 import 'regenerator-runtime/runtime'
 import { App } from './app'
-import { ConfigurationResponse, MoviesSearchResponse, NowPlayingResponse } from './shared/model/model-results'
-import { Movie, Genre, MovieDetailsMoreCollection, ReviewsDetails } from './shared/model/model-common'
-import { Utils } from './shared/services/utils-service'
 
 async function init() {
-  // loadDOM()
   const theApp = new App({ apiKey: 'bc50218d91157b1ba4f142ef7baaa6a0' })
-  const utils = new Utils()
 
-  // get references to dom nodes
-  const moviesParentNode: HTMLElement = document.getElementById('movies') as HTMLElement
-  const searchBtnNode: HTMLInputElement = document.getElementById('movies-search') as HTMLInputElement
-  const moviesNowhBtnNode: HTMLButtonElement = document.getElementById('movies-now') as HTMLButtonElement
-  const observerNode: HTMLElement = document.getElementById('infinite-scroll-trigger') as HTMLElement
-  const moviesNode: HTMLUListElement = document.createElement('ul')
   let observer: IntersectionObserver
-
-  let moviesDataPromise: NowPlayingResponse | MoviesSearchResponse
-  let moviesDataResults: Movie[] | undefined
-
-  // the dynamic part of the page, append it to it's parent element
-  moviesParentNode.appendChild(moviesNode)
-
-  /**
-   ** get the configuration data which might be needed. #todo: store in localstorage, as suggested
-   */
-  const configObjPromise: ConfigurationResponse = await theApp.getConfig(theApp.anApiToken)
-  const configObj: string = configObjPromise.images.base_url
-
-  /**
-   * get the genres to lookup
-   */
-  const genres: Genre[] = (await theApp.getGenres(theApp.anApiToken)).genres
-
-  /**
-   *
-   * @param _movies the result list from the queries
-   * this builds the DOM initially and everytime there is a new search
-   */
-  const _buildDOMwithResults = (_movies: Movie[]): void => {
-    // document.dispatchEvent(new CustomEvent('movieDataLoaded', { detail: moviesDataResults }))
-    if (theApp.moviesSearch.query == '') {
-      utils._updatePageRequest(theApp.moviesNowCurent)
-    } else {
-      utils._updatePageRequest(theApp.moviesSearch)
-    }
-
-    // could use a template - #TODO
-    for (const movie of _movies) {
-      const movieLiNode: HTMLLIElement = document.createElement('li')
-      movieLiNode.setAttribute('data-movie-id', movie.id?.toString() || '')
-      movieLiNode.setAttribute('data-movie-link', 'true')
-      moviesNode.appendChild(movieLiNode)
-
-      const fragment: DocumentFragment = document.createDocumentFragment()
-      const childDiv1: HTMLElement = document.createElement('div')
-      const childDiv2: HTMLElement = document.createElement('div')
-      const childDiv3: HTMLElement = document.createElement('div')
-
-      childDiv1.setAttribute('class', 'movie-column with-poster')
-      childDiv1.innerHTML = `<img class="movie-poster responsive" loading="lazy" width="500" height="750" src="${configObj}w500/${movie.poster_path}"/>`
-      fragment.appendChild(childDiv1)
-
-      childDiv2.setAttribute('class', 'movie-column with-info')
-      childDiv2.innerHTML = `<h1 class="movie-title"><a class="override-link" target="blank" href="https://www.themoviedb.org/movie/${
-        movie.id
-      }">${movie.title?.trim()}</a><span class="movie-date">(${utils._getYear(
-        movie.release_date
-      )})</span><span class="movie-more">...more</span></h1><span class="movie-genres">${utils._getGenreTitle(
-        movie.genre_ids,
-        genres
-      )}</span><p>${movie.overview}</p><p class="movie-stars">${utils._getStars(movie.vote_average)}</p>`
-      fragment.appendChild(childDiv2)
-
-      childDiv3.setAttribute('class', 'movie-column with-more-info')
-      fragment.appendChild(childDiv3)
-
-      movieLiNode.appendChild(fragment)
-    }
-  }
-
-  const _getMoreMovies = async () => {
-    moviesDataPromise = await theApp.getMoviesNow(theApp.moviesNowCurent)
-    moviesDataResults = moviesDataPromise.results
-    if (moviesDataResults) {
-      _buildDOMwithResults(moviesDataResults)
-    }
-  }
-
-  const _getMoreSearchedMovies = async () => {
-    moviesDataPromise = await theApp.getMoviesSearch(theApp.moviesSearch)
-    moviesDataResults = moviesDataPromise.results
-    if (moviesDataResults) {
-      _buildDOMwithResults(moviesDataResults)
-    }
-  }
-
-  const _addMovieMoreContent = (_data: MovieDetailsMoreCollection, _desired: HTMLElement): void => {
-    let _innerHTML = ''
-    const fragment: DocumentFragment = document.createDocumentFragment()
-    const childDiv: HTMLElement = document.createElement('div')
-    childDiv.setAttribute('class', 'more-info-panel')
-    const { reviews, similar, videos } = _data
-
-    // childDiv.innerHTML = _addMovieMoreContent(_data)
-    // _desired.querySelector('.with-more-info').innerHTML = _addMovieMoreContent(_movieData)
-
-    if (videos.results && videos.results.length > 0) {
-      for (const video of videos.results) {
-        _innerHTML += `<iframe width="420" height="315" src="https://www.${video.site}.com/embed/${video.key}"></iframe>`
-      }
-    }
-    if (reviews.results && reviews.results.length > 0) {
-      _innerHTML += '<div>'
-      _innerHTML += '<h1>Reviews</h1>'
-      const limitedReviews: ReviewsDetails[] = []
-      limitedReviews.push(reviews.results[0])
-      if (reviews.results[1]) limitedReviews.push(reviews.results[1])
-      for (const review of limitedReviews) {
-        _innerHTML += `<h3>from ${review.author} </h3><p>${review.content}</p>`
-      }
-      _innerHTML += '</div>'
-    }
-    if (similar.results && similar.results.length > 0) {
-      _innerHTML += '<div>'
-      _innerHTML += '<h1>Similar movies</h1>'
-      for (const simil of similar.results) {
-        _innerHTML += `<h3><a target="_blank" href="https://www.themoviedb.org/movie/${simil.id}"> ${simil.title}</a> </h3>`
-      }
-      _innerHTML += '</div>'
-    }
-
-    childDiv.innerHTML = _innerHTML
-    fragment.appendChild(childDiv)
-    _desired?.querySelector('.with-more-info')?.appendChild(fragment)
-  }
-  /**
-   *
-   * @param evt
-   * @returns the selected movie details. we will be adding a single event listener which will delegate the event
-   * instead of adding one for every movie which will have a performance penalty. as an eastern egg, you can
-   * click anywhere on the movie and get the results but this will not be indicated
-   *
-   */
-  const _showMoreDetails = async (evt: Event): Promise<void | null> => {
-    const _el = evt.target as HTMLElement
-    const _desired: HTMLElement = _el.closest('[data-movie-link]') as HTMLElement
-    const _movieId = _desired?.getAttribute('data-movie-id')
-    let _movieData: MovieDetailsMoreCollection
-    theApp.setmovieMoreDetails(_movieId || '')
-    if (_desired?.classList.contains('show-more')) {
-      _desired.classList.remove('show-more')
-      return null
-    } else {
-      if (_desired.querySelector('.with-more-info')?.innerHTML == '') {
-        _movieData = await theApp.getMovieMore(theApp.moviesMoreDetails)
-        console.log(_movieData)
-        _addMovieMoreContent(_movieData, _desired)
-      }
-      _desired?.classList.toggle('show-more')
-    }
-  }
-  moviesNode.addEventListener('click', _showMoreDetails)
-
-  // document.addEventListener('movieDataLoaded', ((event: CustomEvent) => {
-  //   console.log(event.detail)
-  // }) as EventListener)
-
-  const _getSearchedMovies = async (searchString: string): Promise<void> => {
-    theApp.moviesSearch.query = searchString
-    theApp.moviesSearch.pageNo = 1
-    moviesDataPromise = await theApp.getMoviesSearch(theApp.moviesSearch)
-    moviesDataResults = moviesDataPromise.results
-    if (moviesDataResults && moviesDataResults.length > 0) {
-      moviesNode.innerHTML = ''
-      _buildDOMwithResults(moviesDataResults)
-    } else {
-      theApp.moviesSearch.query = ''
-      theApp.moviesSearch.pageNo = 1
-      alert('no results')
-    }
-  }
-
   /**
    * we will allow the user some time between keypresses and actually activating the search
    */
-  const _searchDebounced = utils.debounce(function (evt: Event): void {
+  const _searchDebounced = theApp.utils.debounce(function (evt: Event): void {
     // allow only searches with at least 3 chars, although this will exclude some, ie 'IT', 'ET'
     // but can mitigated by pressing enter thouggh this might be somewhat confusing.. i ll see
-    if (searchBtnNode.value.match(/\w{3}/)) {
+    if (theApp.searchBtnNode.value.match(/\w{3}/)) {
       // sanitise input
-      _getSearchedMovies(searchBtnNode.value.trim().replace(/[\.'"\*\+-@#]/g, ''))
+      theApp._getSearchedMovies(theApp.searchBtnNode.value.trim().replace(/[\.'"\*\+-@#]/g, ''))
     }
   }, 250)
-  searchBtnNode.addEventListener('keyup', _searchDebounced)
+
+  theApp.moviesNode.addEventListener('click', theApp._showMoreDetails)
+  theApp.searchBtnNode.addEventListener('keyup', _searchDebounced)
 
   /**
    * need to have a way to go back to original results
    */
-  moviesNowhBtnNode.addEventListener('click', () => {
+  theApp.moviesNowhBtnNode.addEventListener('click', () => {
     if (theApp.moviesSearch.query !== '') {
       theApp.moviesSearch.query = ''
       theApp.moviesSearch.pageNo = 1
       theApp.moviesNowCurent.pageNo = 1
-      moviesNode.innerHTML = ''
+      theApp.moviesNode.innerHTML = ''
     }
   })
   /**
@@ -219,9 +43,9 @@ async function init() {
         setTimeout(() => {
           // you can either load more now playing or more searched
           if (theApp.moviesSearch.query == '') {
-            _getMoreMovies()
+            theApp._getMoreMovies()
           } else {
-            _getMoreSearchedMovies()
+            theApp._getMoreSearchedMovies()
           }
         }, 1000)
       }
@@ -234,8 +58,10 @@ async function init() {
    *! the list explitictly but perhaps a solution which would use listeners to attach the event
    *! after everything was ready would be better
    */
-  // getMoreMovies()
-  observer.observe(observerNode)
+
+  observer.observe(theApp.observerNode)
+  theApp._getConfig()
+  theApp._getGenres()
 }
 
 init()
